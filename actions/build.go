f79a9570feff9f696510d6f91f2ff322a5e02c13
Pablo Morelli <pabmorelli@gmail.com>
2023-10-27 19:07:35 +0200

    init actions

---
+++ actions/build.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"context"
+)
+
+func Build(ctx context.Context) error {
+	println("build")
+	return nil
+}
---
+++ actions/go.mod
@@ -0,0 +1,3 @@
+module github.com/pmorelli92/shuttle-plan-actions
+
+go 1.21.3
